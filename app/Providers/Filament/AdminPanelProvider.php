7f49908bd202f2b7f3a49fe607af597a3c385fe5
sheikhahmer <sheikhahmer1999@gmail.com>
2025-10-14 08:20:46 +0500

    remiove srapte

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -54,7 +54,7 @@ class AdminPanelProvider extends PanelProvider
                 DispatchServingFilamentEvent::class,
             ])
             ->plugins([
-                FilamentShieldPlugin::make(),
+
             ])
             ->authMiddleware([
                 Authenticate::class,
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,3 @@ Route::get('/', function () {
 Route::get('/orders/{order}/print/kitchen', [OrderPrintController::class, 'kitchen'])->name('orders.print.kitchen');
 Route::get('/orders/{order}/print/receipt', [OrderPrintController::class, 'receipt'])->name('orders.print.receipt');
 Route::get('/orders/{order}/print/paid', [OrderPrintController::class, 'paid'])->name('orders.print.paid');
-
-Route::get('/reports/item-sales-category/download', [ReportController::class, 'downloadCategoryReport'])
-    ->name('reports.item_sales.category.download');
-
-Route::get('/reports/item-sales-category/print', [ReportController::class, 'printCategoryReport'])
-    ->name('reports.item_sales.category.print');
